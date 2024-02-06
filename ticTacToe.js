68754cff0a997401e9bf7da3129425cc7e2596da
Hazem Abdulmoneim <hazem.mando408@gmail.com>
2024-02-06 19:28:53 +0200

    complete logic for winning the game

---
+++ ticTacToe.js
@@ -0,0 +1,76 @@
+const Gameboard = (function () {
+  const boardState = [null, null, null, null, null, null, null, null, null];
+
+  const winningPositions = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ];
+
+  return {
+    boardState,
+    winningPositions,
+  };
+})();
+
+const newPlayer = (name, mark) => {
+  return { name, mark };
+};
+
+const newGame = (player1, player2) => {
+  const drawMark = (player, position) => {
+    Gameboard.boardState[position - 1] = player.mark;
+    if (checkForwin(player.mark)) {
+      win(player);
+    } else if (checkForDraw(position)) {
+      draw();
+    }
+  };
+
+  const checkForwin = (mark) => {
+    const sameMarkPositions = [];
+    for (let i = 0; i < boardState.length; i++) {
+      if (boardState[i] === mark) {
+        sameMarkPositions.push(i + 1);
+      }
+    }
+
+    for (let i = 0; i < Gameboard.winningPositions.length; i++) {
+      if (
+        Gameboard.winningPositions[i].every((el) =>
+          sameMarkPositions.includes(el)
+        )
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const win = (player) => {
+    console.log(`${player.name} has won the game!`);
+  };
+
+  const draw = () => {
+    console.log("The game has ended in a draw.");
+  };
+
+  return {
+    player1,
+    player2,
+  };
+};
+
+const test = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const indices = [];
+for (let i = 0; i < test.length; i++) {
+  if (test[i] % 2 == 0) {
+    indices.push(i);
+  }
+}
