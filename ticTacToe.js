a159e6c76d968bebd9d6a4d2fc601a3e4f2cfab2
Hazem Abdulmoneim <hazem.mando408@gmail.com>
2024-02-10 21:22:46 +0200

    finished the logic

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,5 +1,18 @@
+const emptyBoard = () => {
+  return new Array(9).fill(null);
+};
+
+const start = document.querySelector("#startBtn");
+
+start.addEventListener("click", (event) => {
+  event.preventDefault();
+  const player1Name = document.querySelector(".player1Name");
+  const player2Name = document.querySelector(".player2Name");
+  newGame(newPlayer(player1Name.value, "x"), newPlayer(player2Name.value, "o"));
+});
+
 const Gameboard = (function () {
-  const boardState = [null, null, null, null, null, null, null, null, null];
+  const boardState = emptyBoard();
 
   const winningPositions = [
     [1, 2, 3],
@@ -18,21 +31,71 @@ const Gameboard = (function () {
   };
 })();
 
+const drawBoard = () => {
+  const container = document.querySelector(".container");
+  container.innerHTML = "";
+  for (let i = 0; i < Gameboard.boardState.length; i++) {
+    const div = document.createElement("div");
+    div.addEventListener("click", () => {
+      alert("Please choose the names of the players first.");
+    });
+    container.appendChild(div);
+  }
+};
+
 const newPlayer = (name, mark) => {
   return { name, mark };
 };
 
+addEventListener("load", () => {
+  drawBoard();
+});
+
 const newGame = (player1, player2) => {
-  drawBoard(Gameboard.boardState);
+  let currentTurn = player1;
+  const updateBoard = (boardState) => {
+    const container = document.querySelector(".container");
+    container.innerHTML = "";
+    for (let i = 0; i < boardState.length; i++) {
+      const div = document.createElement("div");
+      div.addEventListener("click", () => {
+        if (Gameboard.boardState[i]) {
+          alert("Position already taken!");
+          return;
+        }
+        drawMark(currentTurn, i + 1);
+        if (currentTurn == player1) currentTurn = player2;
+        else currentTurn = player1;
+      });
+      if (!boardState[i]) {
+        div.innerText = "";
+      } else {
+        div.innerText = boardState[i];
+      }
+      container.appendChild(div);
+    }
+  };
+  for (let i = 1; i <= 9; i++) {
+    if (i % 2 != 0) {
+      currentTurn = player1;
+    } else {
+      currentTurn = player2;
+    }
+    updateBoard(Gameboard.boardState);
+  }
+
+  updateBoard(Gameboard.boardState);
   const drawMark = (player, position) => {
     Gameboard.boardState[position - 1] = player.mark;
-    drawBoard(Gameboard.boardState);
-    if (checkForwin(player.mark)) {
-      win(player);
-    }
-    if (!Gameboard.boardState.includes(null)) {
-      draw();
-    }
+    updateBoard(Gameboard.boardState);
+    setTimeout(() => {
+      if (checkForwin(player.mark)) {
+        win(player);
+      }
+      if (!Gameboard.boardState.includes(null)) {
+        draw();
+      }
+    }, 100);
   };
 
   const checkForwin = (mark) => {
@@ -55,32 +118,21 @@ const newGame = (player1, player2) => {
   };
 
   const win = (player) => {
-    console.log(`${player.name} has won the game!`);
+    alert(`${player.name} has won the game!`);
+    clearBoard();
   };
 
   const draw = () => {
-    console.log("The game has ended in a draw.");
+    alert("The game has ended in a draw.");
+    clearBoard();
+  };
+
+  const clearBoard = () => {
+    Gameboard.boardState = emptyBoard();
   };
 
   return {
     player1,
     player2,
-    drawMark,
   };
-};
-
-const drawBoard = (boardState) => {
-  const container = document.querySelector(".container");
-  container.innerHTML = "";
-  for (let i = 0; i < boardState.length; i++) {
-    if (boardState[i] == null) {
-      const div = document.createElement("div");
-      div.innerText = "";
-      container.appendChild(div);
-    } else {
-      const div = document.createElement("div");
-      div.innerText = boardState[i];
-      container.appendChild(div);
-    }
-  }
 };
